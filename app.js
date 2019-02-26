8cd3168af96678efb909a74a373c63faa77703f6
dcomittag <dcomittag@gmail.com>
2019-02-26 22:07:56 +0700

    I currently work / study here button
    not styled yet

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 
 const app = new Vue({
   el:'#app',
-  data() {
-
-    return {
+  data: {
       name: '',
       current_position: '',
       current_company: '',
@@ -32,47 +30,47 @@ const app = new Vue({
       location1: '',
       startdate1: '',
       enddate1: '',
-      noend1: '',
+      noend1: false,
       tasks1: '',
       position2: '',
       company2: '',
       location2: '',
       startdate2: '',
       enddate2: '',
-      noend2: '',
+      noend2: false,
       tasks2: '',
       position3: '',
       company3: '',
       location3: '',
       startdate3: '',
       enddate3: '',
-      noend3: '',
+      noend3: false,
       tasks3: '',
       study_s1: '',
       institution_s1: '',
       location_s1: '',
       startdate_s1: '',
       enddate_s1: '',
-      noend_s1: '',
+      noend_s1: false,
       achievements_s1: '',
       study_s2: '',
       institution_s2: '',
       location_s2: '',
       startdate_s2: '',
       enddate_s2: '',
-      noend_s2: '',
+      noend_s2: false,
       achievements_s2: '',
       study_s3: '',
       institution_s3: '',
       location_s3: '',
       startdate_s3: '',
       enddate_s3: '',
-      noend_s3: '',
+      noend_s3: false,
       achievements_s3: '',
       max: 100,
-      valuebar: 10,
-    }
+      valuebar: 10
   },
+
   computed: {
 /*    currently: function() {
   if (this.current_position && this.current_company && this.current_location) {
@@ -161,7 +159,7 @@ const app = new Vue({
       } else if (this.enddate_s1) {
         return 'until ' + this.enddate_s1;
       } else if (this.noend_s1) {
-        return 'I currently work here.';
+        return 'I currently study here.';
       } else {
         return null;
       }
@@ -181,7 +179,7 @@ const app = new Vue({
       } else if (this.enddate_s2) {
         return 'until ' + this.enddate_s2;
       } else if (this.noend_s2) {
-        return 'I currently work here.';
+        return 'I currently study here.';
       } else {
         return null;
       }
@@ -201,7 +199,7 @@ const app = new Vue({
       } else if (this.enddate_s3) {
         return 'until ' + this.enddate_s3;
       } else if (this.noend_s3) {
-        return 'I currently work here.';
+        return 'I currently study here.';
       } else {
         return null;
       }
@@ -209,7 +207,24 @@ const app = new Vue({
 
   },
   methods: {
-
+    iworkhere1() {
+      this.noend1 = !this.noend1;
+    },
+    iworkhere2() {
+      this.noend2 = !this.noend2;
+    },
+    iworkhere3() {
+      this.noend3 = !this.noend3;
+    },
+    istudyhere1() {
+      this.noend_s1 = !this.noend_s1;
+    },
+    istudyhere2() {
+      this.noend_s2 = !this.noend_s2;
+    },
+    istudyhere3() {
+      this.noend_s3 = !this.noend_s3;
+    },
 /*     progressivo: function() {
        if (this.name = true && this.email = true) {
          return
